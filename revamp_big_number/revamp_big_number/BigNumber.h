e77534f40658a6b8ceef1f4972e098ea8146e027
M.Saleh <mega_unknown@hotmail.com>
2020-08-21 20:30:29 -0500

    created Header File for BigNumber

---
+++ revamp_big_number/revamp_big_number/BigNumber.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef __BIG_NUMBER_H__
+#define __BIG_NUMBER_H__
+
+#endif // !__BIG_NUMBER_H__
--- revamp_big_number/revamp_big_number/revamp_big_number.cpp
+++ revamp_big_number/revamp_big_number/revamp_big_number.cpp
@@ -1,7 +1,5 @@
 // revamp_big_number.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-#include "pch.h"
 #include "pch.h"
 #include <iostream>
 #include <windows.h>
